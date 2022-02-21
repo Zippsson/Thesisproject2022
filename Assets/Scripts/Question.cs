771739a4be391a54a84f0967e403fb9944860f3d
Zippsson <62258314+Zippsson@users.noreply.github.com>
2022-02-21 11:19:15 +0100

    Added sprites and win canvas

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Question : MonoBehaviour
+{
+    [SerializeField] GameObject correctAnswer;
+    [SerializeField] GameObject wrongAnswer;
+    [SerializeField] GameObject flag;
+
+
+
+
+
+}
