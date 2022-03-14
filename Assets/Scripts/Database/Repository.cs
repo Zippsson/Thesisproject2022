a66efb28f62a8d5503d1d32cc34a42440c9d7980
Zippsson <62258314+Zippsson@users.noreply.github.com>
2022-03-14 09:50:08 +0100

    Initial commit of Database function

---
+++ Assets/Scripts/Database/Repository.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityNpgsql;
+
+public class Repository : MonoBehaviour
+{
+    NpgsqlConnection conn;
+    private string connection = "Server = pgserver.mah.se; User Id = aj7870; Password = mau; database = aj7870";
+    
+}
